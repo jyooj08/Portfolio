f239d9c261eaf8900b1e3e044376a64ad6c124f1
Yujin Jang <jyooj08@gmail.com>
2020-10-21 00:31:21 +0900

    Javascript navbar scroll

--- main.js
+++ main.js
@@ -15,10 +15,33 @@ document.addEventListener('scroll', function(){
 const navbar = document.querySelector('#navbar');
 const navbar_height = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY, '/', navbar_height);
+    //console.log(window.scrollY, navbar_height, document.documentElement.scrollTop);
     if(window.scrollY > navbar_height){
         navbar.classList.add('navbar--dark');
     } else{
         navbar.classList.remove('navbar--dark');
     }
+});
+
+// Handle scrolling when tapping on the navbar menu
+/*const home_position = document.querySelector('#home').scrollTop;
+const home_link = document.querySelector('li[0]');
+home_link.addEventListener('click', ()=>{
+    console.log('click');
+    window.scrollTo(0, home_position)
+});*/
+const navbar_menu = document.querySelector('.navbar__menu');
+navbar_menu.addEventListener('click', (event)=>{
+    const target = event.target;
+    const link = target.dataset.link;
+
+    if(link == null) return;
+
+    const scroll_to = document.querySelector(link);
+    scroll_to.scrollIntoView({behavior: 'smooth'});
+    /*
+    --Yujin's solution!--
+    const scroll_to = document.querySelector(link).offsetTop;
+    window.scrollTo({top:scroll_to - 40, behavior: 'smooth'});
+    */
 });
